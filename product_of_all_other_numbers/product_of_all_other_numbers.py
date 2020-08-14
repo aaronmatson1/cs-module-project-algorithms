77eb8949823f71a09011d50fd398594d78362ccb
aaronmatson1 <matsonaaron1@gmail.com>
2020-08-13 23:15:46 -0600

    Product PASSED

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,42 +3,19 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    # Your code here
+    # I need to start with 1, because if you multiply by 0, you're just gonna get more 0's.
+    result = 1
 
-    from functools import reduce
+    for x in arr: # Start a for loop to loop through our array, and multiply each value with the result.
+        result *= x # Once we multiply, we will have a new result. Keep multiplying until we run out of elements in the array
+    return [result / x for x in arr] # our result will then be divided the number in our list
+    # Using the first arr = [1, 2, 3, 4, 5] it will return a result of 120.
+    # 120 will then be divided by each number in our list, giving us an output of [120, 60, 40, 30, 24]
 
-    #return [reduce(lambda x,y: x*y, [j for j in arr if j != i]) for i in arr]
-
-    forward = [1]
-    backward = [1]
-    tally = 1
-    for i in range(len(arr)-1):
-        tally = tally * arr[i]
-        forward.append(tally)
-
-
-    arr.reverse()
-
-    tally = 1
-    for i in range(len(arr)-1):
-        tally = tally * arr[i]
-        backward.append(tally)
-
-    backward.reverse()
-
-    output = list(map(lambda x,y:x*y,forward,backward))
-
-    return output
-
-# [1], [1]*[2], [1]*[2]*[3], [1]*[2]*[3]*[4], [1]*[2]*[3]*[4]*[5]
-# [5], [5]*[4], [5]*[4]*[3], [5]*[4]*[3]*[2], [5]*[4]*[3]*[2]*[1]
-
-# 1, [1], [1]*[2], [1]*[2]*[3], [1]*[2]*[3]*[4]
-# 1, [5], [5]*[4], [5]*[4]*[3], [5]*[4]*[3]*[2] - reverse this then elementwise multiply
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
      arr = [1, 2, 3, 4, 5]
     # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
